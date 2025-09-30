11d61f872337dc1a1cfa2786165fd380fb314567
Caio H. R. R. <caiohrr@gmail.com>
2025-09-30 00:23:14 -0300

    Adicionando graficos de comparação e textos cifrados-decifrados

--- plot_graphs.py
+++ plot_graphs.py
@@ -1,25 +1,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# Example data (3 comparisons, 2 values each)
-labels = ['Comparison 1', 'Comparison 2', 'Comparison 3']
-group1 = [10, 15, 7]   # Values for first set
-group2 = [12, 9, 11]   # Values for second set
+labels = ['Carta - São José de Anchieta', 'Categories - Aristotle', 'The Republic - Plato']
+# fib_cifra = [0.005274, 0.026209, 0.309004]
+# aes_cifra = [0.003669, 0.005245, 0.006711]
+
+
+fib_decifra = [0.005283, 0.024466, 0.296683]
+aes_decifra = [0.003536, 0.003246, 0.005493]
+
 
 x = np.arange(len(labels))  # positions for comparisons
 width = 0.35  # bar width
 
-fig, ax = plt.subplots()
-bars1 = ax.bar(x - width/2, group1, width, label='Group 1')
-bars2 = ax.bar(x + width/2, group2, width, label='Group 2')
+fig, ax = plt.subplots(figsize=(10, 6)) 
+bars1 = ax.bar(x - width/2, fib_decifra, width, label='Fib Decifra')
+bars2 = ax.bar(x + width/2, aes_decifra, width, label='AES Decifra')
 
-# Labels and title
-ax.set_ylabel('Values')
-ax.set_title('Bar Chart with 3 Comparisons')
+ax.set_ylabel('Tempo (s)')
+ax.set_title('Comparação de decifras')
 ax.set_xticks(x)
-ax.set_xticklabels(labels)
+ax.set_xticklabels(labels, rotation=15, ha='right')  
 ax.legend()
 
-# Auto scale is default, but we can ensure layout fits
+# Mostrar os valores em cima das barras
+ax.bar_label(bars1, fmt="%.6f", padding=3)
+ax.bar_label(bars2, fmt="%.6f", padding=3)
+
 plt.tight_layout()
 plt.show()
